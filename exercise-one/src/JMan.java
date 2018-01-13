443acea1bd639af4f9591d5ae0869693ff446129
Matthew Ralph <ralpmat@gmail.com>
2018-01-13 10:28:32 +0000

    Did ex 1

---
+++ exercise-one/src/JMan.java
@@ -0,0 +1,17 @@
+public class JMan {
+  private String name;
+  public int age;
+
+  public JMan(String name) {
+    this.name = name;
+    this.age = 147; // wow they're old
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
--- exercise-one/src/Reflect.kt
+++ exercise-one/src/Reflect.kt
@@ -1,8 +1,17 @@
+import kotlin.reflect.KClass
+
+class Dog {
+    fun bark() = println("Woof!")
+}
+
 fun main(args: Array<String>) {
     if (args.size != 1) {
         println("Requires a class name as argument to program")
         System.exit(-1)
     }
+    val c: Class<*> = Class.forName(args[0])
+    val k: KClass<*> = Class.forName(args[0]).kotlin
+    c.methods.forEach { println(it) }
+    k.constructors.forEach { println(it) }
 
-    // TODO
 }
